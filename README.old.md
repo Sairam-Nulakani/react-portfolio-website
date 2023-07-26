4fabc4257e7bbad77a3214ca5ac3fa5659d30e31
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-07-26 18:08:21 +0530

    created all components

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-portfolio-website
+> React Portfolio website
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+    </>
+  );
+}
+
+export default App;
