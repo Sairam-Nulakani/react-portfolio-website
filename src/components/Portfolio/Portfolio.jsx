0655ff3c302b74b0e877ea31b48824664d3b3962
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-07-27 19:01:04 +0530

    updated portfolio page

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import "./portfolio.css";
+import ECOM from "../../assets/e-commerce.webp";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  const data = [
+    {
+      id: 1,
+      image: ECOM,
+      title: "E-Commerce Website",
+      github: "https://github.com/Sairam-Nulakani?tab=repositories",
+      demo: "https://github.com/Sairam-Nulakani?tab=repositories",
+    },
+    {
+      id: 2,
+      image: ECOM,
+      title: "Food Order App",
+      github: "https://github.com/Sairam-Nulakani?tab=repositories",
+      demo: "https://github.com/Sairam-Nulakani?tab=repositories",
+    },
+    {
+      id: 3,
+      image: ECOM,
+      title: "Resume Builder App",
+      github: "https://github.com/Sairam-Nulakani?tab=repositories",
+      demo: "https://github.com/Sairam-Nulakani?tab=repositories",
+    },
+  ];
+  return (
+    <section id="portfolio">
+      <h5>Portfolio</h5>
+      <h2>My Recent Work</h2>
+      <div className="container portfolio__container">
+        {data.map((item) => (
+          <article className="portfolio__item" key={item.id}>
+            <div className="portfolio__item-image">
+              <img src={item.image} alt={item.title} />
+            </div>
+            <h3>{item.title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={item.github} className="btn" target="_blank">
+                GitHub
+              </a>
+              <a href={item.demo} className="btn btn-primary" target="_blank">
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
